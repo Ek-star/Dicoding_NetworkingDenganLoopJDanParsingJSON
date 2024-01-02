3ada57cc564045d06c522a5ba172797aad68f040
Ekalma Toto Alloy Sembiring <totoekalma@gmail.com>
2024-01-02 15:04:49 +0700

    Last Commit

--- app/src/main/java/com/example/myquote/MainActivity.kt
+++ app/src/main/java/com/example/myquote/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.example.myquote
 
+import ListQuotesActivity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
@@ -25,6 +27,10 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         getRandomQuote()
+
+        binding.btnAllQuotes.setOnClickListener {
+            startActivity(Intent(this@MainActivity, ListQuotesActivity::class.java))
+        }
     }
 
     private fun getRandomQuote() {
@@ -35,22 +41,29 @@ class MainActivity : AppCompatActivity() {
             override fun onSuccess(statusCode: Int, headers: Array<Header>, responseBody: ByteArray) {
                 // Jika koneksi berhasil
                 binding.progressBar.visibility = View.INVISIBLE
+
                 val result = String(responseBody)
                 Log.d(TAG, result)
                 try {
                     val responseObject = JSONObject(result)
+
                     val quote = responseObject.getString("en")
                     val author = responseObject.getString("author")
+
                     binding.tvQuote.text = quote
                     binding.tvAuthor.text = author
+
                 } catch (e: Exception) {
                     Toast.makeText(this@MainActivity, e.message, Toast.LENGTH_SHORT).show()
                     e.printStackTrace()
                 }
+
             }
-            override fun onFailure(statusCode: Int, headers: Array<Header>, responseBody: ByteArray, error: Throwable) {
+
+            override fun onFailure(statusCode: Int, headers: Array<Header>?, responseBody: ByteArray, error: Throwable) {
                 // Jika koneksi gagal
                 binding.progressBar.visibility = View.INVISIBLE
+
                 val errorMessage = when (statusCode) {
                     401 -> "$statusCode : Bad Request"
                     403 -> "$statusCode : Forbidden"
