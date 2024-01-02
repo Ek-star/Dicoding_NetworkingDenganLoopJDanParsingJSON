9dfd1a4217160f665ca44f5c5c93d0362e581402
Ekalma Toto Alloy Sembiring <totoekalma@gmail.com>
2024-01-02 08:56:11 +0700

    Mengatur dan menambahkan implementation pada gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -38,6 +38,7 @@ android {
 dependencies {
 
     implementation("androidx.core:core-ktx:1.12.0")
+    implementation("com.loopj.android:android-async-http:1.4.9")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
